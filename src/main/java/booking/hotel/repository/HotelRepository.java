77f045646dd7ccb738cdc8053d3096db3e821747
EUIJOON <euijoonism@gmail.com>
2023-11-08 13:32:45 +0900

    [작업] HotelRepository 구현

--- src/main/java/booking/hotel/repository/HotelRepository.java
+++ src/main/java/booking/hotel/repository/HotelRepository.java
@@ -1,4 +1,26 @@
 package booking.hotel.repository;
 
+import booking.hotel.entity.Hotel;
+import booking.hotel.entity.Room;
+
+import java.util.List;
+
 public class HotelRepository {
+    Hotel hotel = new Hotel();
+
+    public int getAsset(){
+        return hotel.getHotelAsset();
+    }
+
+    public List<Room> getRoomList(){
+        return hotel.getRoomList();
+    }
+
+    public String getAdminPassword(){
+        return hotel.adminPassword();
+    }
+
+    public void setAsset(int price){
+        hotel.setAsset(price);
+    }
 }
